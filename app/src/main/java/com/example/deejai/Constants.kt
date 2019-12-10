04f55794633765a4e1e3df0204766c6ef5fb039f
victor <veetsirrah@gmail.com>
2019-12-10 00:02:46 -0300

    localization fetch

--- app/src/main/java/com/example/deejai/Constants.kt
+++ app/src/main/java/com/example/deejai/Constants.kt
@@ -1,7 +1,7 @@
 package com.example.deejai
 
 object Constants {
-
+    const val LOCALIZATION_CODE = 1
     const val SPOTIFY_CREDENTIALS = "spotify credentials"
     const val SPOTIFY_TOKEN = "spotify token"
     const val NO_TOKEN = "no token"
--- app/src/main/java/com/example/deejai/RoomSelection/SelectRoomActivity.kt
+++ app/src/main/java/com/example/deejai/RoomSelection/SelectRoomActivity.kt
@@ -1,13 +1,22 @@
 package com.example.deejai.RoomSelection
-
+import android.Manifest
+import android.content.pm.PackageManager
+import com.example.deejai.Constants.LOCALIZATION_CODE
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.deejai.R
 import com.example.deejai.RoomSelection.SelectRoomContract.ViewModel
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class SelectRoomActivity : AppCompatActivity(), ViewModel {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     lateinit var roomList : RecyclerView
     lateinit var presenter : SelectRoomPresenter
@@ -20,6 +29,23 @@ class SelectRoomActivity : AppCompatActivity(), ViewModel {
         roomList.layoutManager = linearLayoutManager
         presenter = SelectRoomPresenter(this)
         presenter.listAvaiableRooms()
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), LOCALIZATION_CODE)
+        }
+        else{
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location : Location? ->
+                    Log.d("LocalizationString", location.toString())
+                }
+                .addOnFailureListener {
+                    throw Exception(it)
+                }
+        }
     }
 
     override fun showAvaiableRooms(rooms: List<Room>) {
