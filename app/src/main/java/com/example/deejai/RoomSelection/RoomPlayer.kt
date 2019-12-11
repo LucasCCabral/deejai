72495486ffb5d594faae817359b2780defcc095f
victor <veetsirrah@gmail.com>
2019-12-10 23:47:33 -0300

    embedded mocked player in roomplayer activity

--- app/src/main/java/com/example/deejai/RoomSelection/RoomPlayer.kt
+++ app/src/main/java/com/example/deejai/RoomSelection/RoomPlayer.kt
@@ -1,20 +1,21 @@
 package com.example.deejai.RoomSelection
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import android.widget.Toast
-import com.example.deejai.Constants.ROOM_NAME_KEY
+import android.util.DisplayMetrics
+import androidx.appcompat.app.AppCompatActivity
 import com.example.deejai.R
+import kotlinx.android.synthetic.main.activity_room_player.*
+
 
 class RoomPlayer : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_room_player)
-        var bundle :Bundle? = intent.extras
-        var message = bundle!!.getString(ROOM_NAME_KEY)
-        val mytext : TextView = findViewById(R.id.room_name)
-        mytext.text = message
+        webview.getSettings().setJavaScriptEnabled(true)
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        val html = "<iframe src=\"https://open.spotify.com/embed/album/1DFixLWuPkv3KT3TnV35m3\" width=\"350\" height=\"530\" frameborder=\"0\" allowtransparency=\"true\" allow=\"encrypted-media\"></iframe>"
+        webview.loadData(html, "text/html", null)
     }
 }
