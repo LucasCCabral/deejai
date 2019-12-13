40a9dce17804a4cd23372d629a55a850216bcbf0
victor <veetsirrah@gmail.com>
2019-12-13 16:29:09 -0300

    added fetch funcionalities, no testing

--- app/src/main/java/com/example/deejai/RoomSelection/RoomPlayer.kt
+++ app/src/main/java/com/example/deejai/RoomSelection/RoomPlayer.kt
@@ -1,21 +1,51 @@
 package com.example.deejai.RoomSelection
 
+import Song
 import android.os.Bundle
-import android.util.DisplayMetrics
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.deejai.R
-import kotlinx.android.synthetic.main.activity_room_player.*
 
 
 class RoomPlayer : AppCompatActivity() {
+    private var userView: TextView? = null
+    private var songView: TextView? = null
+    private var addBtn: Button? = null
+    private var song: Song? = null
+
+    private var songService: SongService? = null
+    private var recentlyPlayedTracks: ArrayList<Song>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_room_player)
-        webview.getSettings().setJavaScriptEnabled(true)
-        val displayMetrics = DisplayMetrics()
-        windowManager.defaultDisplay.getMetrics(displayMetrics)
-        val html = "<iframe src=\"https://open.spotify.com/embed/album/1DFixLWuPkv3KT3TnV35m3\" width=\"350\" height=\"530\" frameborder=\"0\" allowtransparency=\"true\" allow=\"encrypted-media\"></iframe>"
-        webview.loadData(html, "text/html", null)
+
+        songService = SongService(applicationContext)
+        userView = findViewById<View>(R.id.user) as TextView
+        songView = findViewById<View>(R.id.song) as TextView
+        addBtn = findViewById<View>(R.id.add) as Button
+
+        val sharedPreferences = getSharedPreferences("SPOTIFY", 0)
+        userView!!.setText(sharedPreferences.getString("userid", "No User"))
+
+        getTracks()
+    }
+
+    private fun getTracks() {
+        songService!!.getRecentlyPlayedTracks(object : VolleyCallBack {
+            override fun onSuccess() {
+                recentlyPlayedTracks = songService!!.getSongs()
+                updateSong()
+            }
+        })
+    }
+
+    private fun updateSong() {
+        if (recentlyPlayedTracks!!.size > 0) {
+            songView!!.text = recentlyPlayedTracks!![0].name
+            song = recentlyPlayedTracks!![0]
+        }
     }
 }
---
+++ app/src/main/java/com/example/deejai/RoomSelection/VolleyCallBack.kt
@@ -0,0 +1,5 @@
+package com.example.deejai.RoomSelection
+
+interface VolleyCallBack {
+    fun onSuccess()
+}
